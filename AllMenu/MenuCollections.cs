d0dd9950b3e8e59c6ad6f89e0bd8870a1955f349
Eyvazliamil <167743489+Eyvazliamil@users.noreply.github.com>
2026-07-19 13:34:27 +0400

    Update MenuCollections.cs

--- AllMenu/MenuCollections.cs
+++ AllMenu/MenuCollections.cs
@@ -1,4 +1,4 @@
-﻿using HospitalProject.Appointments;
+using HospitalProject.Appointments;
 using HospitalProject.CustomExceptions;
 using HospitalProject.CV;
 using HospitalProject.EMAIL;
@@ -16,21 +16,23 @@ using System.Runtime.Intrinsics.Arm;
 namespace HospitalProject.AllMenu
 {
     public class MenuCollections
-    {  
+    {
         #region objects and files
         public static Cv cv = new Cv();
-        public static AdminLogin admin = new AdminLogin();
+        public static AdminLogin adminLog = new AdminLogin();       
+        public static AdminPermissions adminPermission = new AdminPermissions();
         public static UserLogin userLogin = new UserLogin();
         public static UserRegistration userReg = new UserRegistration();
         public static List<string> emails = File.Exists("AdminEmail.txt") ? File.ReadAllLines("AdminEmail.txt").ToList() : new List<string>();
         public static DoctorRegistration doctorReg = new DoctorRegistration();
         public static DoctorLogin doctorLog = new DoctorLogin();
+        public static AdminRegistration adminReg = new AdminRegistration();
 
         public static string dentistryDoctor = "dentistryDoctors.json";
         public static string pediatricsDoctor = "pediatricsDoctors.json";
         public static string traumatologyDoctor = "traumatologyDoctors.json";
 
-        public static string appointmentsFile = "appointments.txt"; 
+        public static string appointmentsFile = "appointments.txt";
         #endregion
 
         #region AllMenu
@@ -38,13 +40,16 @@ namespace HospitalProject.AllMenu
         public static string[] appTimeToArray = File.Exists(appointmentsFile) ? File.ReadAllLines(appointmentsFile).ToArray() : Array.Empty<string>();
         public static string[] times = appTimeToArray.Select(x => x.Split(' ')[0]).ToArray();
         public static string[] isAvailable = appTimeToArray.Select(x => x.Split(' ')[1]).ToArray();
+        public static string[] adminLoginPage = { "Admin Registration", "Admin Login", "Back" };
         public static string[] userLoginPage = { "User Registration", "User Login", "Back" };
         public static string[] doctorLoginPage = { "Doctor Registration", "Doctor Login", "Send CV", "Back" };
-        public static string[] mainPage = { "Admin Login", "User Section", "Doctor Section", "Exit" };
-        public static string[] adminPage = { "Delete User","Delete Doctor", "E-mail", "Back" };
+        public static string[] mainPage = { "Admin Section", "User Section", "Doctor Section", "Exit" };
+        public static string[] adminPage = { "Delete User", "Delete Doctor", "Back" };
+        //public static string[] adminPage = { "Delete User", "Delete Doctor", "E-mail", "Back" };
         public static string[] userDoctorPage = { "Make Appointment", "About Doctors", "Back" };
+        public static string[] DoctorOwnPage = { "My Appointment", "Back" };
         public static CvStatus[] CvStatusPage = { CvStatus.Pending, CvStatus.Rejected, CvStatus.Approved };
         #endregion
-    
+
     }
 }
